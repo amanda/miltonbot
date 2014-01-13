760bd339077570071007bdd7d4b3c2b705022772
amandapickering <pickering.amanda@gmail.com>
2014-01-12 20:05:36 -0500

    initial commit

---
+++ milton_bot.py
@@ -0,0 +1,15 @@
+from twython import Twython
+from pymarkovchain import MarkovChain
+
+APP_KEY = 'xuPB9BcyotCdrB5t13K8Q'
+APP_SECRET = 'TtmtMcYrNGpi7fhgr8pDkmpPbUOy9xuhTVulbuM'
+OAUTH_TOKEN = '2285416724-Bz9wfxHPDKFD15ewhirC39upOO9GN4fE8xzHYTM'
+OAUTH_TOKEN_SECRET = 'gC2XSxvur8PfgkHi2C9kAkHQoffWvdMrcZ7l9KWUy4pxt'
+
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+mc = MarkovChain("./markov")
+mc.generateDatabase(open("/Users/ampickering/Dropbox/miltonbot/milton-paradise.txt").read())
+paradiseString = mc.generateString()
+
+twitter.update_status(status=paradiseString)
